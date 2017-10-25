4c59232a5cf2c86c6082b1eca57e5e125378a64b
KiriNicholson <32941940+KiriNicholson@users.noreply.github.com>
2017-10-25 00:00:50 -0400

    add mayb via upload

---
+++ maybe.java
@@ -0,0 +1,14 @@
+
+public class maybe {
+	
+	public String firstmethod() {
+		String s = "Does this work?  Please let it work";
+		System.out.println(s);
+		return s;
+	}
+	
+	public void main(String[] arg) {
+		firstmethod();
+		
+	}
+}
